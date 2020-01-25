891cf867893dab4794b71dda8c367760247c9faa
Charley Reynolds <charleyr42@gmail.com>
2020-01-25 21:52:46 +0000

    feat: Added company details to Contact us page

--- src/laytonstreet/pages/ContactUsPage.tsx
+++ src/laytonstreet/pages/ContactUsPage.tsx
@@ -2,15 +2,35 @@ import * as React from 'react';
 import Page from '../components/Page';
 
 export default function ContactUsPage() {
+    const companyName = "Layton Street Freehold Company Ltd"
+    const companyNumber = "11098547"
+    const registeredAddress = "Mint Lettings Ltd, 162b, Stevenage, England, SG1 3LL";
     const email = "freehold@laytonstreet.co.uk";
     return (
         <Page narrow>
             <h1 className="text-center display-4">Contact us</h1>
             <br/>
             <address>
-               <strong>Email</strong>
-               <br/>
-               <a href={`mailto:${email}`}>{email}</a>
+                <p>
+                    <strong>Company name</strong>
+                    <br/>
+                    {companyName}
+                </p>
+                <p>
+                    <strong>Company number</strong>
+                    <br/>
+                    {companyNumber}
+                </p>
+                <p>
+                    <strong>Registered address</strong>
+                    <br/>
+                    {registeredAddress}
+                </p>
+                <p>
+                    <strong>Email</strong>
+                    <br/>
+                    <a href={`mailto:${email}`}>{email}</a>
+                </p>
             </address> 
         </Page>
     );
